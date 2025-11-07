6d1af1cd25be1f88a69d49699c6857a071298565
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2025-11-08 00:37:06 +0530

    learn pipeline and transaction

--- pub-sub.js
+++ pub-sub.js
@@ -17,21 +17,41 @@ async function tryAddtionalFeatures(){
     await client.connect()
     console.log("Connection established.");
 
-    const subscriber = client.duplicate() //create a new cliet ->shares the same connection
-    await subscriber.connect()
+    // const subscriber = client.duplicate() //create a new cliet ->shares the same connection
+    // await subscriber.connect()
 
-    await subscriber.subscribe('dummy-channel', (message, channel)=>{
-      console.log(`Recieved the message from ${channel}: ${message} `);
+    // await subscriber.subscribe('dummy-channel', (message, channel)=>{
+    //   console.log(`Recieved the message from ${channel}: ${message} `);
       
-    })
-    await client.publish("dummy-channel", "Test data 1")
-    await client.publish("dummy-channel", "Test data 2")
+    // })
+    // await client.publish("dummy-channel", "Test data 1")
+    // await client.publish("dummy-channel", "Test data 2")
 
-    await new Promise((resolve)=>setTimeout(resolve, 1000))
+    // await new Promise((resolve)=>setTimeout(resolve, 1000))
 
-    await subscriber.unsubscribe('dummy-channel')
-    await subscriber.quit() //close the subscriber connection
+    // await subscriber.unsubscribe('dummy-channel')
+    // await subscriber.quit() //close the subscriber connection
 
+
+    //pipelining and transactions
+
+const multi = client.multi();
+multi.set("key-transaction1", "value1");
+multi.set("key-transaction2", "value2");
+multi.get("key-transaction1");
+multi.get("key-transaction2");
+const results = await multi.exec();
+console.log(results); // [ 'OK', 'OK', 'value1', 'value2' ]
+
+const pipeline = client.multi();
+pipeline.set("key-pipeline1", "value1");
+pipeline.set("key-pipeline2", "value2");
+pipeline.get("key-pipeline1");
+pipeline.get("key-pipeline2");
+const pipelineResults = await pipeline.exec();
+console.log(pipelineResults); // [ 'OK', 'OK', 'value1', 'value2' ]
+
+    
 
     
   } catch (err) {
@@ -41,3 +61,4 @@ async function tryAddtionalFeatures(){
       client.quit()
     }
 }
+tryAddtionalFeatures()
